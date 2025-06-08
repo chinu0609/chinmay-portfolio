1c47c83e29ad07641c531422f8c21a2643da7b2c
Chinmay-Bhosale <chinmay.bhosale@appliedaiconsulting.com>
2025-06-08 22:19:06 +0530

    add correct paths

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/chinmay-portfolio"
+  base: "/chinmay-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
