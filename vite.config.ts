3930becfedbce962ea77487c400cc96cc142704e
Chinmay-Bhosale <chinmay.bhosale@appliedaiconsulting.com>
2025-06-08 22:15:36 +0530

    add correct paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace with your repo name exactly as it appears on GitHub
-const repoName = "chinmay-portfolio"; // e.g., "portfolio-site" or "chinmay-portfolio"
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`, // 👈 Required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
@@ -17,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/chinmay-portfolio"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
